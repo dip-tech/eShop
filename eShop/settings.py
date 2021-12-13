f4ccc1724fe6c9850606e8594e8c2a2245c2d6fb
dip-tech <dipankar8dutta@gmail.com>
2021-12-13 23:43:48 +0530

    Commit 2

--- eShop/settings.py
+++ eShop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9s&6p)*rghk+riz5w2_i9=g-&)ubl(2w4bm9n==4u+jhrr&orf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
