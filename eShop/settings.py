26c39ec190fd3cd8c110483cfec94f83cb011a46
dip-tech <dipankar8dutta@gmail.com>
2021-12-14 19:13:27 +0530

    Commit 10

--- eShop/settings.py
+++ eShop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9s&6p)*rghk+riz5w2_i9=g-&)ubl(2w4bm9n==4u+jhrr&orf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["estore-in.herokuapp.com","127.0.0.1:8000"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,10 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS=[
-#     os.path.join(BASE_DIR,'static')
-# ]
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+#STATIC_ROOT=os.path.join(BASE_DIR,'static')
 #media directory for eshop
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
--- eShop/urls.py
+++ eShop/urls.py
@@ -42,4 +42,4 @@ urlpatterns = [
     #order app urls paths...
     path('order/',include('order.urls'))
     
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
